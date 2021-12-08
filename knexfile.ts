daed16e41a3c84a8662bf49dee643a8127f21809
Christian Braz gallo <christianbrazgallo@MacBook-Pro-de-Christian.local>
2021-12-08 13:08:33 -0300

    feat(Knex): refactor knex config to accept heroku

--- knexfile.ts
+++ knexfile.ts
@@ -1,19 +1,19 @@
 require('dotenv').config({  
   path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
 });
-// Update with your config settings.
-// const config = require('./config.ts');
-// const database_settings = config().database;
 
 if(process.env.DATABASE_URL === '') { 
   throw new Error('Inconsistent .env please follow .env.sample')
 }
 
-console.log(process.env.DATABASE_URL);
+console.log(`${process.env.DATABASE_URL}?ssl=true`);
 
 module.exports = {    
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: { 
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },    
     pool: { min: 2, max: 7 },
     debug: true,    
     migrations: {      
