5460f689149e6b8285c5e3d312279e3082a758e3
Christian Braz gallo <christianbrazgallo@MacBook-Pro-de-Christian.local>
2021-12-08 12:55:17 -0300

    feat(Database): add a heroku pg database

--- knexfile.ts
+++ knexfile.ts
@@ -1,8 +1,15 @@
+require('dotenv').config({  
+  path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
+});
 // Update with your config settings.
-const config = require('./config.ts');
-const database_settings = config().database;
+// const config = require('./config.ts');
+// const database_settings = config().database;
 
-console.log(database_settings);
+if(process.env.DATABASE_URL === '') { 
+  throw new Error('Inconsistent .env please follow .env.sample')
+}
+
+console.log(process.env.DATABASE_URL);
 
 module.exports = {    
     client: 'pg',
